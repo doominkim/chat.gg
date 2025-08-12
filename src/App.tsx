616f0820c6a3d6ccf24007e61a26b2e311d177c7
SeobinYun <o0oseobin@naver.com>
2025-08-12 23:22:23 +0900

    feat: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 // App.tsx
-import React from 'react';
-import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
-import UserDetail from './pages/UserDetail';
-import Archive from './pages/Archive';
-import '@cloudscape-design/global-styles/index.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from "react";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import UserDetail from "./pages/UserDetail";
+import Archive from "./pages/Archive";
+import NotFoundUser from "./pages/NotFoundUser";
+import UserList from "./pages/UserList";
+import "@cloudscape-design/global-styles/index.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 const App: React.FC = () => {
   return (
@@ -15,8 +22,10 @@ const App: React.FC = () => {
           {/* 메인 진입 시 자동으로 /dashboard로 이동 */}
           <Route path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<UserDetail />} />
-          <Route path = "/archive" element = {<Archive />} />
+          <Route path="/user-rank" element={<UserList />} />
+          <Route path="/user/:nickname" element={<UserDetail />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/not-found" element={<NotFoundUser />} />
         </Routes>
       </Layout>
     </Router>
